866801798ad701830e624b6638d362ebcd6014ed
Your Name <iyanperezmarcos@gmail.com>
2023-01-17 15:31:16 +0100

    list added

---
+++ src/main/frontend/src/components/List.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">
+        <button><i class="fa-solid fa-xmark"></i></button>
+        <button><i class="fa-solid fa-pen"></i></button>
+        <input id="list_element" type="text" readonly>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
