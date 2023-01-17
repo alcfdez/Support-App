1422416565f347f9c355fc717157deaeefca9dc0
Enol-Igareta <enol1code@gmail.com>
2023-01-17 19:44:08 +0100

    landing page acabada

---
+++ src/main/frontend/src/components/HeadPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="header">
+        <img class="header__logo" src="@/assets/img/logo-factoria.png" alt="logo f5">
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use "@/scss/colors" as c;
+@use "@/scss/mixins" as m;
+    .header{
+        @include m.flex(flex, auto, nowrap, center, center);
+        height: 9em;
+        width: 100%;
+        position: relative;
+        top: 8%;
+        background: map-get(c.$colors, "white");;
+        
+        &__logo{
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+    }
+
+</style>
--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,9 +1,63 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import FootPage from '@/components/FootPage.vue';
+import HeadPage from '@/components/HeadPage.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HeadPage />
+    <nav class="nav">
+      <router-link to="/FormView" class="nav__link nav__link-form" href="">Fill Form Request</router-link>
+
+      <router-link to="/ListView" class="nav__link nav__link-list" href="">List Request</router-link>
+        
+    </nav>
   </main>
+    <FootPage />
 </template>
+
+<style lang="scss" scoped>
+@use "@/scss/colors" as c;
+@use "@/scss/mixins" as m;
+
+  main{
+    height: 100vh;
+    background: url("@/assets/img/fondo.png");
+    background-size: cover;
+    
+    .nav{
+      height: 70%;
+      @include m.flex(flex, column, nowrap, center, center);
+      gap:2em;
+      font-weight: bold;
+
+      &__link{
+        @include m.flex(flex, auto, nowrap, center, center);
+        background: map-get(c.$colors, "orange");
+        @include m.borderRadius();
+        color: map-get(c.$colors, "white");
+        font-size: 2em;
+        -webkit-box-shadow: 0px 18px 13px 6px rgba(56,51,56,0.59);
+        -moz-box-shadow: 0px 18px 13px 6px rgba(56,51,56,0.59);
+        box-shadow: 0px 18px 13px 6px rgba(56,51,56,0.59);
+          &:hover{
+            transition: .3s;
+            background: map-get(c.$colors, "dark-blue");
+            color: map-get(c.$colors, "white");
+    }
+        &-form{
+          height: 3em;
+          width: 20%;
+        }
+
+        &-list{
+          height: 2em;
+          width: 18%;
+        }
+
+      }
+    }
+  }
+
+
+</style>
