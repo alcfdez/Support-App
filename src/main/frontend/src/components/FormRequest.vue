6851e57a81381ab16eb943fcf0555c3aa8dcf736
Caren Salles <carencrisbsalles@gmail.com>
2023-01-18 13:01:15 +0100

    form updated

--- src/main/frontend/src/components/FormRequest.vue
+++ src/main/frontend/src/components/FormRequest.vue
@@ -2,37 +2,33 @@
 
 <template>
     <main>
-        <div class="__container-title">
-            <h2>Fill Form Request</h2>
-        </div>
-
+       
         <form class="__container-forms">
-            <label class="__container-forms__label" for="firstname">First name:</label><br />
-            <input type="text" class="__container-forms__label__inputbox" name="firstname" /><br />
+            <label class="__container-forms__label" for="firstname">First name:</label>
+            <input type="text" class="__container-forms__label__inputbox" name="firstname" />
 
-            <label class="__container-forms__label" for="surname">Surname:</label><br />
-            <input type="text" class="__container-forms__label__inputbox" name="surname" /><br />
+            <label class="__container-forms__label" for="surname">Surname:</label>
+            <input type="text" class="__container-forms__label__inputbox" name="surname" />
 
-            <label class="__container-forms__label" for="date">Data of Request:</label><br />
-            <input type="date" class="__container-forms__label__inputbox" name="date" /><br />
+            <label class="__container-forms__label" for="date">Data of Request:</label>
+            <input type="date" class="__container-forms__label__inputbox" name="date" />
 
-            <label class="__container-forms__label" for="type">Type of Request:</label><br />
+            <label class="__container-forms__label" for="type">Type of Request:</label>
             <select type="text" class="__container-forms__label__inputbox" name="typerequest">
-                <option class= "features" ></option>
+                <option class= "features" selected disabled >Put the type of Request</option>
                 <option class= "features" >Material Request</option>
                 <option class= "features" >Repair Request</option>
                 <option class= "features" >TI Service Request</option>
                 <option class= "features" >SAC</option>
-            </select><br />
+            </select>
 
-            <label class="__container-forms__label" for="type">Description of your Request:</label><br />
-            <input type="text" class="__container-forms__label__inputbox__description" name="description" /><br />
-
+            <label class="__container-forms__label" for="type">Description of your Request:</label>
+            <textarea name="name-description" class="__container-forms__label__inputbox__description" cols="30" rows="5"></textarea>
                 <button class="button__one" type="button">
-                    Submit</button><br />
+                    Submit</button>
                 <div class="container__button">
                     <button class="button__two" type="button">
-                        Reset</button><br />
+                        Reset</button>
                     <button class="button__three" type="button">
                         Cancel
                     </button>
@@ -47,49 +43,41 @@ main {
     width: 100%;
     height: 100%;
     background-color: rgb(204, 199, 199);
-    
-}
-
-.__container-title {
-    background-color: orangered;
-    margin: 3em 27.8em 0.5em;
-    width: 15em;
-    height: 3em;
-    border-radius: 0.5em;
     display: flex;
     justify-content: center;
-    align-items: center;
-    font-size: 1.2em;
-    font-weight: bold;
-    color: white;
+
+    
 }
 
 .__container-forms {
     width: 30%;
-    margin: 1em 30em 5em;
     background-color: white;
     padding: 2em;
     border-radius: 1em;
     font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+   
 
     &__label {
         background-color: white;
-        margin-left: 1em;
-
+        align-self: start;
+       
+        
         &__inputbox {
             border-radius: 1em;
             height: 2em;
-            width: 20em;
+            width: 80%;
             padding: 0.5em;
             background-color: rgb(160, 158, 158);
-            margin-left: .7em;
+            margin-bottom: 1em;
         
             &__description {
-                height: 10em;
-                width: 20.1em;
+                
                 background-color:rgb(160, 158, 158);
                 border-radius: 1em;
-                margin-left: .7em;
+                margin-bottom: 1em;
             }
         }
     }
@@ -100,17 +88,16 @@ main {
     background-color: orangered;
     border-radius: 1em;
     padding: 0.7em;
-    width: 18em;
+    width: 80%;
     display: flex;
     justify-content: center;
-    margin-left: 1.6em;
     color: white;
+    margin-bottom: 1em;
 }
 .container__button{
     width: 50%;
     display: flex;
     justify-content: center;
-    margin-left: 5.2em;
     gap: 1em;
     color: white;
     
@@ -123,10 +110,10 @@ main {
     }
 .button__three{
     border: .1em solid orangered;
-        border-radius: 1em;
-        padding: .7em;
-        font-size: 1em;
-        color: orangered;
+    border-radius: 1em;
+    padding: .7em;
+    font-size: 1em;
+    color: orangered;
     }
 
 
