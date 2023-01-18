855f2afc60e43625bde2d4974f4222961c5daa60
Your Name <iyanperezmarcos@gmail.com>
2023-01-18 14:17:24 +0100

    faked data

--- src/main/frontend/src/components/List.vue
+++ src/main/frontend/src/components/List.vue
@@ -4,7 +4,11 @@
 
 <template>
     <ul>
-        <li v-for="(item, index) in items" :key="index">
+        <li>
+        <p>nombre y apellidos tiriti</p>
+        <p>10/10/1000</p>
+        <p>tipo de solicitud lorena ipsum</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, nesciunt. Pariatur deserunt sed atque incidunt ducimus laudantium enim doloremque earum, ab repellat molestiae temporibus aliquid est velit voluptate? Architecto, facere. Nobis odit delectus fugit ipsum repudiandae dolores quasi? Minus tempore, suscipit laborum voluptates perferendis hic. Distinctio rem aperiam molestiae dolorem.</p>
         <button><i class="fa-solid fa-xmark"></i></button>
         <button><i class="fa-solid fa-pen"></i></button>
         <input id="list_element" type="text" readonly>
