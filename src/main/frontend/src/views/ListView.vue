cb413a4f7111d5ce9204567ab3e7f3b84c645eed
blake S <design.harls@icloud.com>
2023-01-26 15:39:13 +0100

    fix style list

--- src/main/frontend/src/views/ListView.vue
+++ src/main/frontend/src/views/ListView.vue
@@ -1,30 +1,49 @@
 <script setup>
-import List from '../components/List.vue';
-import {ref,onBeforeMount} from 'vue';
-import Employed from '../services/EmployeeApi.js';
+import List from "../components/List.vue";
+import { ref, onBeforeMount } from "vue";
+import Employed from "../services/EmployeeApi.js";
+import HeadPage from "../components/HeadPage.vue";
+import FootPage from "../components/FootPage.vue";
 
 let serviceApi = new Employed([]);
 
 serviceApi.fetchEmployed;
 let employeds = ref([]);
 
-onBeforeMount(async()=>{
+onBeforeMount(async () => {
   employeds.value = await serviceApi.fetchEmployed();
-  
-  console.log(employeds.value)
-})
+
+  console.log(employeds.value);
+});
 </script>
 
 <template>
-    <ul>
-      <li v-for="employed in employeds">
-      <List :employed="employed"/>
+  <HeadPage />
+  <ul class="wrapper">
+    <li  v-for="employed in employeds">
+      <List :employed="employed" />
       <button><i class="fa-solid fa-xmark"></i></button>
       <button><i class="fa-solid fa-pen"></i></button>
       <input id="list_element" type="text" readonly />
     </li>
   </ul>
+  <FootPage />
 </template>
+<style lang="scss" scoped>
+@use "@/scss/colors" as c;
+@use "@/scss/mixins" as m;
 
-<style scoped>
+.wrapper{
+    height: 100vh;
+    background: url("@/assets/img/fondo.png");
+    background-size: cover;
+
+    
+}
+li{
+    background-color: aquamarine;
+    display: flex;
+    justify-content: center;
+    
+}
 </style>
